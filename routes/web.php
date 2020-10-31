11ba09275beada3e3b83cc53295e2d877eebca9f
Fignocius <diogo_no_vais@hotmail.com>
2020-10-30 23:25:06 -0300

    Adds course route, controller and model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('cursos', CursosController::class);
+Route::resource('cursos', CursoController::class);
